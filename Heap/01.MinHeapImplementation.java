8325539431f995f118334ec973521fcf2e68361a
Amit Kumar <kumrzgc@amazon.com>
2021-01-15 12:50:34 +0530

    Added HEAP directory and Heap01 and heap03

---
+++ Heap/01.MinHeapImplementation.java
@@ -0,0 +1,194 @@
+/*
+JAVA implementation on MIN-HEAP
+
+Operation:
+getMin(): O(1)
+extractMin(): O(Log n)
+insert(): O(Log n)
+
+Min Heap vs. Max Heap: https://www.educative.io/edpresso/min-heap-vs-max-heap
+*/
+
+import java.util.Scanner;
+
+public class MinHeap {
+    private int[] heap;
+    private int heapCurrIndex;
+    private int heapMaxSize;
+
+    MinHeap (int heapMaxSize) {
+        this.heapMaxSize = heapMaxSize;
+        this.heapCurrIndex = 0;
+        heap = new int[heapMaxSize];
+
+        // If Heap array start with 1, this.heapCurrIndex = 1; heap[0] = Integer.MIN_VALUE;
+    }
+
+    public int getParent(int index) {
+        return (index-1)/2;
+        // If heap array start with index 0 then parent will be at: (index-1)/2
+        // If heap array start with index 1 then parent will be at: index/2
+    }
+
+    public int getLeftChild(int index) {
+        return index*2 + 1;
+        // If heap array start with index 0 then left child will be at: index * 2
+        // If heap array start with index 1 then left child will be at: (index * 2) + 1
+    }
+
+    public int getRightChild(int index) {
+        return index*2 + 2;
+        // If heap array start with index 0 then right child will be at: (index * 2) + 1
+        // If heap array start with index 1 then right child will be at: (index * 2) + 2
+    }
+
+    public boolean checkIsLeafNode(int index) {
+        return (index >= (heapCurrIndex / 2) && index <= heapCurrIndex);
+    }
+
+    // Swaps two nodes of the heap
+    public void swapElements(int index1, int index2) {
+        int temp = heap[index1];
+        heap[index1] = heap[index2];
+        heap[index2] = temp;
+    }
+
+    public void insertInHeap(int element) {
+        if (heapCurrIndex >= heapMaxSize) {
+            System.out.println("Heap is at its max size!! Create a new HEAP to insert new elements.");
+            return;
+        }
+
+        heap[heapCurrIndex] = element;
+        int currentHeapIndex = heapCurrIndex;
+
+        while (heap[currentHeapIndex] < heap[getParent(currentHeapIndex)]) {
+            swapElements(currentHeapIndex, getParent(currentHeapIndex));
+            currentHeapIndex = getParent(currentHeapIndex);
+        }
+        heapCurrIndex++;
+    }
+
+    public void printHeap() {
+        // This prints the index of heap array.
+        for (int i=0; i<heapCurrIndex; i++) {
+            System.out.print("[" + i + "]" + " ");
+        }
+        System.out.println();
+
+        // This prints the data of heap array.
+        for (int i=0; i<heapCurrIndex; i++) {
+            System.out.print(" " + heap[i] + " ");
+        }
+        System.out.println();
+    }
+
+    // Function to heapify the node at index
+    public void minHeapify(int index) {
+        if (!checkIsLeafNode(index)) {
+            if (heap[index] > heap[getLeftChild(index)] || heap[index] > heap[getRightChild(index)]) {
+                if (heap[getLeftChild(index)] > heap[getRightChild(index)]) {
+                    swapElements(index, getRightChild(index));
+                    minHeapify(getRightChild(index));
+                } else {
+                    swapElements(index,getLeftChild(index));
+                    minHeapify(getLeftChild(index));
+                }
+            }
+        }
+    }
+
+    public int removeFromHeap() {
+        if (heapCurrIndex <= 0) {
+            return -1;
+        }
+
+        int element = heap[0];
+        //Think why we are doing --heapCurrIndex and not heapCurrIndex--
+        heap[0] = heap[--heapCurrIndex];
+        minHeapify(0);
+        return element;
+    }
+
+    public int deleteIndex(int index) {
+        // Think why used heap_size-1 and not heap_size
+        if (heapCurrIndex-1 < index) {
+            return -1;
+        }
+
+        /*
+        Algo:
+        1. Replace index 1 element with Integer.MIN_VALUE
+        2. Make it a HEAP (by check parent-child property)
+        3. As its a HEAP, Integer.MIN_VALUE will come in TOP, i.e. 0 index
+        4. extractMin();
+        */
+
+        int element = heap[index];
+        heap[index] = Integer.MIN_VALUE;
+        while(index > 0 && heap[getParent(index)] > heap[index]) {
+            swapElements(index, getParent(index));
+            index = getParent(index);
+        }
+        removeFromHeap();
+        return element;
+    }
+
+    // builds the min-heap using the minHeapify()
+    // Not been used in current Code, but if we are given an array and we need to make that a heap
+    // then we will be using this function.
+    public void minHeap() {
+        for (int index = ((heapCurrIndex - 1) / 2); index >= 1; index--) {
+            minHeapify(index);
+        }
+    }
+
+    static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.print("Enter size of array that stores the HEAP : ");
+        int enterHeapMaxSize = scanner.nextInt();
+        MinHeap minHeap = new MinHeap(enterHeapMaxSize);
+
+        while (true) {
+            System.out.println("1. INSERT an element in MIN-HEAP");
+            System.out.println("2. REMOVE an element from MIN-HEAP");
+            System.out.println("3. DELETE an index(element) from MIN-HEAP Array");
+            System.out.println("4. EXIT from program");
+            int option = scanner.nextInt();
+
+            switch (option) {
+                case 1:
+                    System.out.print("Enter the element you want to INSERT in heap : ");
+                    int element = scanner.nextInt();
+                    minHeap.insertInHeap(element);
+                    System.out.println("Current HEAP -> ");
+                    minHeap.printHeap();
+                    break;
+
+                case 2:
+                    int value = minHeap.removeFromHeap();
+                    if (value != -1)
+                        System.out.println("Removed element from the HEAP is : " + value);
+                    else
+                        System.out.println("HEAP is empty!!!");
+                    System.out.println("Current HEAP -> ");
+                    minHeap.printHeap();
+                    break;
+
+                case 3:
+                    System.out.print("Enter the INDEX you want to DELETE from the heap : ");
+                    int index = scanner.nextInt();
+                    int valueD  = minHeap.deleteIndex(index);
+                    if (valueD != -1)
+                        System.out.println("Index : " + index + " been deleted from HEAP, its value was : " + valueD);
+                    else
+                        System.out.println("NO such INDEX present in the current HEAP array!!!");
+                    minHeap.printHeap();
+                    break;
+
+                case 4:
+                    System.exit(0);
+            }
+        }
+    }
+}
---
+++ Heap/03.BinaryHeapOperations.java
@@ -0,0 +1,108 @@
+/*
+Practice Portal: https://practice.geeksforgeeks.org/problems/operations-on-binary-min-heap/1
+*/
+
+
+/************************************************** START ********************************************************/
+
+// User function Template for Java
+class MinHeap {
+    int[] harr;
+    int capacity;
+    int heap_size;
+
+    MinHeap(int cap) {
+        heap_size = 0;
+        capacity = cap;
+        harr = new int[cap];
+    }
+    int parent(int i) { return (i - 1) / 2; }
+    int left(int i) { return (2 * i + 1); }
+    int right(int i) { return (2 * i + 2); }
+
+    // THIS funtion() WAS WRITTEN BY ME
+    int extractMin() {
+
+        if (heap_size <= 0) {
+            return -1;
+        }
+
+        int element = harr[0];
+        harr[0] = harr[--heap_size];
+        MinHeapify(0);
+        return element;
+    }
+
+    // THIS funtion() WAS WRITTEN BY ME
+    void insertKey(int k) {
+        harr[heap_size] = k;
+        int currentHeapIndex = heap_size;
+
+        while (harr[currentHeapIndex] < harr[parent(currentHeapIndex)]) {
+            swapElements(currentHeapIndex, parent(currentHeapIndex));
+            currentHeapIndex = parent(currentHeapIndex);
+        }
+        heap_size++;
+    }
+
+    // THIS funtion() WAS WRITTEN BY ME
+    void deleteKey(int i) {
+        // Think why used heap_size-1 and not heap_size
+        if (heap_size-1 < i) {
+            return;
+        }
+
+        /*
+        Algo:
+        1. Replace index 1 element with Integer.MIN_VALUE
+        2. Make it a HEAP (by check parent-child property)
+        3. As its a HEAP, Integer.MIN_VALUE will come in TOP, i.e. 0 index
+        4. extractMin();
+        */
+
+        harr[i] = Integer.MIN_VALUE;
+        while(i > 0 && harr[parent(i)] > harr[i]) {
+            swapElements(i, parent(i));
+            i = parent(i);
+        }
+        extractMin();
+    }
+
+    // THIS funtion() WAS ADDED BY ME
+    // Swaps two nodes of the harr
+    void swapElements(int index1, int index2) {
+        int temp = harr[index1];
+        harr[index1] = harr[index2];
+        harr[index2] = temp;
+    }
+
+    // Decrease key operation, helps in deleting the element
+    void decreaseKey(int i, int new_val) {
+        harr[i] = new_val;
+        while (i != 0 && harr[parent(i)] > harr[i]) {
+            int temp = harr[i];
+            harr[i] = harr[parent(i)];
+            harr[parent(i)] = temp;
+            i = parent(i);
+        }
+    }
+
+    /* You may call below MinHeapify function in
+      above codes. Please do not delete this code
+      if you are not writing your own MinHeapify */
+    void MinHeapify(int i) {
+        int l = left(i);
+        int r = right(i);
+        int smallest = i;
+        if (l < heap_size && harr[l] < harr[i]) smallest = l;
+        if (r < heap_size && harr[r] < harr[smallest]) smallest = r;
+        if (smallest != i) {
+            int temp = harr[i];
+            harr[i] = harr[smallest];
+            harr[smallest] = temp;
+            MinHeapify(smallest);
+        }
+    }
+}
+
+/************************************************** END ********************************************************/
