8c42803286348d4c0f9310cc7bac66ebc76e08f7
amitkumar <amit2197kumar@gmail.com>
2021-01-26 18:49:43 +0530

    Added DP 08 and 09

---
+++ Dynamic Programming/08. Unbounded Knapsack.java
@@ -0,0 +1,112 @@
+/*
+Company Tags: Amazon Google
+Practice Portal:
+Geeksforgeeks: https://practice.geeksforgeeks.org/problems/knapsack-with-duplicate-items4201/1
+*/
+
+// PARENT PROBLEM:
+
+/************************************************** Recursive Solution ************************************************/
+// In recursion, at every index, we have 2 choices, either to pick the current VAL or leave the current VAL
+// If we pick Current VAL we can pick that again, as duplicate are allowed, so we do recursive call with same index
+
+class Solution{
+    //Recursive Solution
+    static int knapSack(int N, int W, int val[], int wt[])  {
+        if (W <= 0)
+            return 0;
+        if (N <= 0)
+            return 0;
+
+        if (W >= wt[N-1]) {
+            return Math.max(knapSack(N-1, W, val, wt),
+                    knapSack(N, W-wt[N-1], val, wt) + val[N-1]);
+        } else {
+            return knapSack(N-1, W, val, wt);
+        }
+    }
+}
+
+/****************************************** Memoization Solution (Top-Down) *******************************************/
+
+/*
+Recursive Solution can be converted into Memoization Solution
+1. We pass a Memoization matrix with -1 filled.
+2. Recursive Solution remain exactly same, just added one if() to check if we have alredy solved this subproblem,
+if TRUE return its value directly,
+if FALSE, then after getting ans save in the Memoization matrix.
+*/
+
+class Solution{
+    //Memoization Solution (Top-Down) : Recursive Call + DP Matrix
+    static int knapSack(int N, int W, int val[], int wt[])  {
+
+        int[][] dp = new int[N+1][W+1];
+        for (int i=0; i<N+1; i++) {
+            for (int j=0; j<W+1; j++) {
+                dp[i][j] = -1;
+            }
+        }
+        return knapSackHelper(N, W, val, wt, dp);
+    }
+
+    static int knapSackHelper(int N, int W, int val[], int wt[], int dp[][]) {
+        if (W <= 0)
+            return 0;
+        if (N <= 0)
+            return 0;
+
+        if (dp[N][W] != -1)
+            return dp[N][W];
+
+        if (W >= wt[N-1]) {
+            dp[N][W] = Math.max(knapSackHelper(N-1, W, val, wt, dp),
+                                knapSackHelper(N, W-wt[N-1], val, wt, dp) + val[N-1]);
+        } else {
+            dp[N][W] = knapSackHelper(N-1, W, val, wt, dp);
+        }
+
+        return dp[N][W];
+    }
+}
+
+/************************************************** Bottom-up Solution ************************************************/
+
+/*
+Memoization Solution (Top-Down) can be converted into Bottom-up Solution
+ROW are always n i.e. Size of the array.
+1. We create a Bottom-up DP matrix.
+2. Bottom-up DP matrix inicialization is done based on Base condition for recursion
+3. From Memoization Solution
+    3.1. n -> i
+    3.2. W -> j
+*/
+
+class Solution{
+    //Bottom-up Solution
+    static int knapSack(int N, int W, int val[], int wt[])
+    {
+        int row = N+1;
+        int col = W+1;
+
+        int[][] dp = new int[row][col];
+        for (int i=0; i<row; i++) {
+            for (int j=0; j<col; j++)
+                dp[i][j] = 0;
+        }
+
+        for (int i=1; i<row; i++) {
+            for (int j=1; j<col; j++) {
+
+                if (j >= wt[i-1]) {
+                    dp[i][j] = Math.max(dp[i-1][j],
+                                        dp[i][j-wt[i-1]] + val[i-1]);
+                } else {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+
+        return dp[row-1][col-1];
+    }
+}
---
+++ Dynamic Programming/09. Coin Change.java
@@ -0,0 +1,117 @@
+/*
+Company Tags:  Accolite Amazon Microsoft Morgan Stanley OYO Rooms Paytm Samsung Snapdeal
+Practice Portal:
+Geeksforgeeks: https://practice.geeksforgeeks.org/problems/coin-change2448/1
+*/
+
+// PARENT PROBLEM:
+
+/************************************************** Recursive Solution ************************************************/
+// In recursion, at every index, we have 2 choices, either to pick the current VAL or leave the current VAL
+// If we pick Current VAL we can pick that again, as duplicate are allowed, so we do recursive call with same index
+
+class Solution
+{
+    public long count(int coin[], int index, int target)
+    {
+        if (index<=0)
+            return 0;
+        if (target<=0)
+            return 1;
+
+        if (target<coin[index-1])
+            return count(coin,index-1,target);
+        else
+            return count(coin,index-1,target) +
+                    count(coin,index,target-coin[index-1]);
+    }
+}
+
+/****************************************** Memoization Solution (Top-Down) *******************************************/
+
+/*
+Recursive Solution can be converted into Memoization Solution
+1. We pass a Memoization matrix with -1 filled.
+2. Recursive Solution remain exactly same, just added one if() to check if we have alredy solved this subproblem,
+if TRUE return its value directly,
+if FALSE, then after getting ans save in the Memoization matrix.
+*/
+
+class Solution
+{
+    public long count(int coin[], int index, int target)
+    {
+        long[][] dp = new long[index+1][target+1];
+        for (int i=0; i< index+1; i++) {
+            for (int j=0; j< target+1; j++) {
+
+                dp[i][j] = -1L;
+            }
+        }
+
+        return coinCount(coin, index, target, dp);
+    }
+
+    public long coinCount(int coin[], int index, int target, long dp[][])
+    {
+        if (index<=0)
+            return 0;
+        if (target<=0)
+            return 1;
+
+        if (dp[index][target] != -1)
+            return dp[index][target];
+
+        if (target<coin[index-1])
+            dp[index][target] = coinCount(coin, index-1, target, dp);
+        else
+            dp[index][target] = coinCount(coin, index-1, target, dp) +
+                    coinCount(coin, index, target-coin[index-1], dp);
+
+        return dp[index][target];
+    }
+}
+
+/************************************************** Bottom-up Solution ************************************************/
+
+/*
+Memoization Solution (Top-Down) can be converted into Bottom-up Solution
+ROW are always n i.e. Size of the array.
+1. We create a Bottom-up DP matrix.
+2. Bottom-up DP matrix inicialization is done based on Base condition for recursion
+3. From Memoization Solution
+    3.1. n -> i
+    3.2. W -> j
+*/
+
+class Solution
+{
+    public long count(int coin[], int index, int target)
+    {
+        int row = index+1;
+        int col = target+1;
+
+        long[][] dp = new long[row][col];
+
+        for (int i=0; i<row; i++){
+            dp[i][0] = 1;
+        }
+
+        for (int i=1; i<col; i++){
+            dp[0][i] = 0;
+        }
+
+
+        for (int i=1; i<row; i++) {
+            for (int j=1; j<col; j++) {
+                if (j<coin[i-1]) {
+                    dp[i][j] = dp[i-1][j];
+                } else {
+                    dp[i][j] = dp[i-1][j] + dp[i][j-coin[i-1]];
+                }
+            }
+        }
+
+        return dp[row-1][col-1];
+    }
+}
