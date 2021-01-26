d11b97c5b4ee8165fe30981294ce70a7f2b54a48
amitkumar <amit2197kumar@gmail.com>
2021-01-26 13:46:52 +0530

    Updated 07 DP

--- Dynamic Programming/07. Target Sum.java
+++ Dynamic Programming/07. Target Sum.java
@@ -6,19 +6,45 @@ LeetCode: https://leetcode.com/problems/target-sum/
 
 // PARENT PROBLEM: Count number of subset with given difference & Count of subsets with sum equal to X
 
+/*
+APPROCH:
+It will be helpfull if we read this first: [Count number of subset with given difference]
+Lets take an example first:
+
+Input: nums is [1, 1, 1, 1, 1], S is 3.
+Output: 5
+Explanation:
+
+-1+1+1+1+1 = 3
++1-1+1+1+1 = 3
++1+1-1+1+1 = 3
++1+1+1-1+1 = 3
++1+1+1+1-1 = 3
+
+If we see carefully, we can say that we are actully splitting the array in 2 subarrays
+1st subarray that are with positive sign, 2nd subarray that are with negative sign
+-1+1+1+1+1 = 3  -> [-1] + [+1+1+1+1]
++1-1+1+1+1 = 3  -> [-1] + [+1+1+1+1]
++1+1-1+1+1 = 3  -> [-1] + [+1+1+1+1]
++1+1+1-1+1 = 3  -> [-1] + [+1+1+1+1]
++1+1+1+1-1 = 3  -> [-1] + [+1+1+1+1]
+
+We are actually breaking the array in 2 subarray, S1 and S2 (one is positive, one is negative)
+and adding both we need to get the TARGET_SUM
+So we can conclude that we want S1 - S2 = TARGET_SUM
+This is the same problem that we discussed in [Count number of subset with given difference]
+For rest of the approch, please read that file: 06.Count number of subset with given difference.md
+
+*/
+
 /************************************************** Recursive Solution ************************************************/
 
 class Solution {
     public int findTargetSumWays(int[] arr, int target) {
         int sum = 0;
-        int count0 = 0;
-        Arrays.sort(arr);
 
         for(int i=0; i<arr.length; i++) {
             sum += arr[i];
-
-            if (arr[i] == 0)
-                count0++;
         }
 
         if (target > sum)
@@ -30,18 +56,17 @@ class Solution {
 
         subSetSum = subSetSum/2;
 
-        int subSetSumCount = subSetSumHelper(arr, subSetSum, arr.length);
-
-        return (int)Math.pow(2,count0) * subSetSumCount;
+        return subSetSumHelper(arr, subSetSum, arr.length);
     }
 
     //Recursive Solution
     public int subSetSumHelper(int[] arr, int sum, int currIndex) {
-        if (sum == 0)
-            return 1;
-
-        if (currIndex <= 0)
-            return 0;
+        if (currIndex <= 0) {
+            if (sum == 0)
+                return 1;
+            else
+                return 0;
+        }
 
         if (sum >= arr[currIndex-1]) {
             return subSetSumHelper(arr, sum, currIndex-1) +
@@ -54,116 +79,4 @@ class Solution {
 
 /****************************************** Memoization Solution (Top-Down) *******************************************/
 
-class Solution {
-    public int findTargetSumWays(int[] arr, int target) {
-        int sum = 0;
-        int count0 = 0;
-        Arrays.sort(arr);
-
-        for(int i=0; i<arr.length; i++) {
-            sum += arr[i];
-
-            if (arr[i] == 0)
-                count0++;
-        }
-
-        if (target > sum)
-            return 0;
-
-        int subSetSum = (target + sum);
-        if (subSetSum%2!=0)
-            return 0;
-
-        subSetSum = subSetSum/2;
-
-        int[][] dp = new int[arr.length + 1][subSetSum + 1];
-
-        for (int i=0; i<arr.length+1; i++) {
-            for (int j=0; j<subSetSum+1; j++) {
-                dp[i][j] = -1;
-            }
-        }
-
-        int subSetSumCount = subSetSumHelper(arr, subSetSum, arr.length, dp);
-
-        return (int)Math.pow(2,count0) * subSetSumCount;
-    }
-
-    //Memoization Solution (Top-Down) : Recursive Call + DP Matrix
-    public int subSetSumHelper(int[] arr, int sum, int currIndex, int[][] dp) {
-        if (sum == 0)
-            return 1;
-        if (currIndex <= 0)
-            return 0;
-
-        if (dp[currIndex][sum] != -1)
-            return dp[currIndex][sum];
-
-        if (sum >= arr[currIndex-1]) {
-            dp[currIndex][sum] = subSetSumHelper(arr, sum, currIndex-1, dp) +
-                    subSetSumHelper(arr, sum-arr[currIndex-1], currIndex-1, dp);
-        } else {
-            dp[currIndex][sum] = subSetSumHelper(arr, sum, currIndex-1, dp);
-        }
-
-        return dp[currIndex][sum];
-    }
-}
-
 /************************************************** Bottom-up Solution ************************************************/
-
-class Solution {
-    public int findTargetSumWays(int[] arr, int target) {
-        int sum = 0;
-        int count0 = 0;
-        Arrays.sort(arr);
-
-        for(int i=0; i<arr.length; i++) {
-            sum += arr[i];
-
-            if (arr[i] == 0)
-                count0++;
-        }
-
-        if (target > sum)
-            return 0;
-
-        int subSetSum = (target + sum);
-        if (subSetSum%2!=0)
-            return 0;
-
-        subSetSum = subSetSum/2;
-
-        int subSetSumCount = subSetSumHelper(arr, subSetSum, arr.length);
-
-        return (int)Math.pow(2,count0) * subSetSumCount;
-    }
-
-    //Bottom-up Solution
-    public int subSetSumHelper(int[] arr, int sum, int currIndex) {
-        int row = currIndex+1;
-        int col = sum+1;
-
-        int[][] dp = new int[row][col];
-
-        for (int i=0; i<row; i++)
-            dp[i][0] = 1;
-
-        for (int j=1; j<col; j++)
-            dp[0][j] = 0;
-
-
-        for (int i=1; i<row; i++) {
-            for (int j=1; j<col; j++) {
-                if ( j >= arr[i-1]) {
-                    dp[i][j] = dp[i-1][j] +
-                            dp[i-1][j-arr[i-1]];
-                } else {
-                    dp[i][j] = dp[i-1][j];
-                }
-            }
-        }
-
-        return dp[row-1][col-1];
-    }
-}
